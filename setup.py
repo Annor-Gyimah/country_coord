47793dbee4721f2daa32b255520700600d036066
Annor-Gyimah <eannor707@gmail.com>
2024-05-15 17:24:36 +0000

    'sm'

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='country-coordinates',
-    version='0.1',
+    version='0.2',
     packages=find_packages(),
     author='Emmanuel Gyimah Annor',
     author_email='eannor707@gmail.com',
     description='Get longitude and latitude coordinates of countries that you can use with xarray.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/country-coordinates',  # Update this to your repository URL
+    url='https://github.com/Annor-Gyimah/country_coord',  
     keywords=['python','coordinates','countries'],
     classifiers=[
         'Development Status :: 3 - Alpha',
